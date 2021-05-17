8707858dddc12315624710199ac86a75030a3b2b
vijay4251 <vijaygsm94@gmail.com>
2021-05-17 13:38:50 +0530

    git demo12

---
+++ src/test/java/Gitdemo/Gitdemo/App1.java
@@ -0,0 +1,14 @@
+package Gitdemo.Gitdemo;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+	
+	public void addedMethod()
+	{
+		System.out.println("new method");
+	}
+
+}
